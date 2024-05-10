ccde52d7ebe23622727c6b67fa7c51f4f2c69cb0
fawazabdganiyu <fawazabdganiyu@gmail.com>
2024-05-10 02:37:20 +0100

    Create express API

---
+++ controller/AppController.js
@@ -0,0 +1,14 @@
+import redisClient from '../utils/redis';
+import dbClient from '../utils/db';
+
+export default class AppController {
+  static getStatus(req, res) {
+    res.status(200).send({ redis: redisClient.isAlive(), db: dbClient.isAlive() });
+  }
+
+  static async getStats(req, res) {
+    const users = await dbClient.nbUsers();
+    const files = await dbClient.nbFiles();
+    res.status(200).send({ users, files });
+  }
+}
